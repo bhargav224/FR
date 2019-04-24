c9e56f791bd59c4a1e31f18972a430bb4ed7aef1
bhargav224 <myjava224@gmail.com>
2019-04-24 17:18:40 +0530

    done

---
+++ FR/src/fr/InviteEmail.java
@@ -0,0 +1,86 @@
+
+package fr;
+import java.util.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name="Invite_Email")
+public class InviteEmail {
+    
+    
+    @Id
+    @GeneratedValue
+    private int id;
+    
+     @Column(nullable = false,length = 255)
+    private String email;
+    
+    
+      @org.hibernate.annotations.Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+   @ManyToOne(targetEntity=User.class,fetch = FetchType.LAZY)
+   @JoinColumn(name="user_id",referencedColumnName ="id",nullable = true)
+    private User user;
+
+    public InviteEmail() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final InviteEmail other = (InviteEmail) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "InviteEmail{" + "id=" + id + ", email=" + email + '}';
+    }
+      
+      
+      
+      
+      
+    
+}
